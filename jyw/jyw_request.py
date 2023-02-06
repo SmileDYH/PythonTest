34a82fc01f2d0eab51845b37da8d5fa09db9fa68
dyh <574916501@qq.com>
2023-02-06 17:21:57 +0800

    新增jyw_request，自动化登录 及 获取保存试题

--- jyw/jyw_request.py
+++ jyw/jyw_request.py
@@ -1,44 +1,19 @@
-# coding: utf-8
-import warnings
-warnings.filterwarnings('ignore')
-from urllib import request
-import urllib.request
-import json
-
-def get(token, url):
-            """请求头"""
-            herders = {'authorization': token}
-            """请求"""
-            req = request.Request(url, None, herders)
-            """响应"""
-            res = request.urlopen(req)
-            html = res.read()
-            print(res.getcode())
-
-            return html.decode("utf-8")
-
-def post(token, url, data):
-            """请求头"""
-            herders = {'authorization': token}
-            """请求"""
-            if (token != None):
-                req = request.Request(url, data, herders)
-            else:
-                req = request.Request(url, data)
-            """响应"""
-            res = request.urlopen(req)
-            html = res.read()
-            print(res.getcode())
-
-            return html.decode("utf-8")
+from utils import request_util
+import jyw_async
+import asyncio
 
 
-def post_new(url, params):
-    params = json.dumps(params)
-    headers = {'Accept-Charset': 'utf-8', 'Content-Type': 'application/json'}
-    # 用bytes函数转换为字节
-    params = bytes(params, 'utf8')
+"""登录 -- java"""
+for i in range(2,4):
+    url = "http://localhost:8088/jyw/login"
+    params = {'apiID': '630ea04a-5dc6-4dd0-9bb9-e9806255dda3',
+                'userID': "tiku" + str(i),
+                'userPwd': 'Abc123456',
+                'apiKey': 'c6dcfd230231467b86fd8cd5e869a05e'}
+    token = "Token " + request_util.post_new(url, params)
+    print(token)
 
-    req = urllib.request.Request(url=url, data=params, headers=headers, method='POST')
-    response = urllib.request.urlopen(req).read()
-    return response.decode('utf-8')
+    """获取并保存试题 -- java"""
+    # 通过异步方式，调用消息队列
+    url = "http://localhost:8088/jyw/messageProduction"
+    asyncio.run(jyw_async.func1(token, url))
